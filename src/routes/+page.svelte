b2db30b1687beb6ac83adfa3f4e46fe68ea39fd6
Daniel Morris (1000112241) <MORRDJ1@student.op.ac.nz>
2023-08-01 11:55:07 +1200

    minor changes to nav

--- src/routes/+page.svelte
+++
@@ -1,8 +0,0 @@
-<nav style="background-color: black;">
-    <img src="icon.png" width="170" alt="Broken">
-    <ul style="display:inline">
-        <li style="display:inline">Ships</li>
-        <li style="display:inline">Modules</li>
-        <li style="display:inline">Weapons</li>
-    </ul>
-</nav>
---
+++ src/routes/index/+page.svelte
@@ -0,0 +1,17 @@
+<nav style="background-color: black; color: white; display: inline-block;">
+    <img href="index" src="icon.png" width="200" alt="Broken">
+    <ul style="display:inline">
+        <li>
+            <a href="index">Home</a>
+        </li>
+        <li>
+            <a href="ships" style="display:inline">Ships</a>
+        </li>
+        <li>
+            <a href="modules" style="display:inline">Modules</a>
+        </li>
+        <li>
+            <a href="weapons" style="display:inline">Weapons</a>
+        </li>
+    </ul>
+</nav>
