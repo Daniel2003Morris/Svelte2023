0d794e076d7ca34e4e6120dc1d04d95384fd797f
Daniel Morris (1000112241) <MORRDJ1@student.op.ac.nz>
2023-08-25 09:51:57 +1200

    grid layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,3 +4,8 @@
         <source src="indexVideo.mp4" type="video/mp4" alt="Unable to play media">
     </video>
 </div>
+<div id="grid-container">
+    <div id="item1"><img src="galnet1.jpg" alt="broken"></div>
+    <div id="item2"><img src="galnet2.png" alt="broken"></div>
+    <div id="item3"><img src="galnet3.png" alt="broken"></div>  
+  </div>
