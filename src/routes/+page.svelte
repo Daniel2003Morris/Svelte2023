730e05654efb04eede57053ca9bea90c0e7e6ccc
Daniel Morris (1000112241) <MORRDJ1@student.op.ac.nz>
2023-10-31 14:54:45 +1300

    started news grid

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,11 @@
 <video autoplay muted loop id="indexVideo">
     <source src="indexVideo.mp4" type="video/mp4" alt="Unable to play media">
 </video>
+<news>
+    <news-image>
+        this
+    </news-image>
+    <news-text>
+        that
+    </news-text>
+</news>
