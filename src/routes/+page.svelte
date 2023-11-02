9180a187395b70de29a0905e6eb2dcb4b6818319
Daniel Morris (1000112241) <MORRDJ1@student.op.ac.nz>
2023-11-03 11:25:19 +1300

    added grid and image to index page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,7 @@
 <video autoplay muted loop id="indexVideo">
     <source src="indexVideo.mp4" type="video/mp4" alt="Unable to play media">
 </video>
-<news>
-    <news-image>
-        this
-    </news-image>
-    <news-text>
-        that
-    </news-text>
-</news>
+<div id="grid-container">
+    <div id="item2"><img src="news-image.png"></div>
+    <div id="item3">here</div>
+</div>
