9cdf8384576b2b6ffe29bfbf46ef4b6b97d34089
Daniel Morris (1000112241) <MORRDJ1@student.op.ac.nz>
2023-11-03 10:44:45 +1300

    added tab details

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,6 @@
+<svelte:head>
+    <title>Index</title> 
+</svelte:head>
 <video autoplay muted loop id="indexVideo">
     <source src="indexVideo.mp4" type="video/mp4" alt="Unable to play media">
 </video>
