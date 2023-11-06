a1a13f10bee2e6393f1d72f6e566d79ea1c63711
Daniel Morris (1000112241) <MORRDJ1@student.op.ac.nz>
2023-11-07 10:32:42 +1300

    adapted code for grid

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,6 @@
     <source src="indexVideo.mp4" type="video/mp4" alt="Unable to play media">
 </video>
 <div id="grid-container">
-    <div id="item2"><img src="news-image.png"></div>
-    <div id="item3">here</div>
+    <div id="news-img"><img src="news-image.png"></div>
+    <div id="news-txt">here</div>
 </div>
