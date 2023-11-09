e4d3dc930ab3ad627f381d25a4a4bae0c957c7a4
unknown <daniel.2003.morris@gmail.com>
2023-11-09 23:17:26 +1300

    printed out variables to contracts page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,21 @@
+<script>
+    import {onMount} from "svelte";
+    let news = [];
+    onMount(async () => {
+        let res = await fetch ('https://gist.githubusercontent.com/Daniel2003Morris/d8fc523007d436e4915232704038d307/raw/5d54145c2ca5a50250fddcd659c393cd59731ff6/galnet.json')
+        news = await res.json();
+        console.log(news)
+    })
+</script>
 <video autoplay muted loop id="indexVideo">
     <source src="indexVideo.mp4" type="video/mp4" alt="Unable to play media">
 </video>
 <div id="index-border"></div>
 <div id="grid-container">
-    <div id="news-img"><img src="news-image.png"></div>
+    <div id="news-img"><img src="{news.img}"></div>
     <div id="news-txt">
-        Commanders have been asked to help destroy the rogue Grey Swan pilots killing civilians in the Shamash system.
-
-        Military squadrons taking part in Fleet Exercise Grey Swan became irrationally violent following an unexplained mass hysteria that swept through Garrett Orbital during a fleet training exercise. Captain Ulrich Maurer, commanding officer of the San Tu State Network megaship Khonsu Gleam, broadcast the following message:
-        <br>
-        “Shamash Future’s security forces have asked us to help stop the carnage taking place throughout this system. No medical or technological solution has been found for affected pilots, who seem reluctant to leave Shamash. Therefore, to prevent the loss of more innocent lives, there is regrettably no choice but to eliminate every ship in the Grey Swan fleet.”
-        <br>
-        “We are urgently seeking to recruit independent pilots with combat experience. Financial rewards and other incentives will be available at the Khonsu Gleam. Hopefully we can perform this unpleasant task as efficiently as possible.”
-        <br>
-        Wing Commander Mackenzie Coates was coordinating Fleet Exercise Grey Swan when a bizarre signal caused its crews to become homicidal. She told a local newsfeed:
-        <br>
-        “This is a nightmarish situation.
+        {console.log(news.)}
+        {news.article}
     </div>
     <div id="news-txt">
         placeholder
